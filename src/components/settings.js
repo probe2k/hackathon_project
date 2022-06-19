cb9af8095c692eb5afede42510fb807f331207e5
ha-wk <ayushkr0369@gmail.com>
2022-06-19 16:55:49 +0530

    settings added

---
+++ src/components/settings.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+    render() {
+        return(
+              
+            <div class="box">
+            
+            <input type="file" id="file" name="image" />
+            <label for="file">Change Picture</label>
+            
+            <input type="text" placeholder="User Name" name="" />
+            <input type="Email" placeholder="Email ID" name="" />
+            <input type="number" placeholder="Phone Number" name="" />
+            <input type="text" name="" placeholder="Gender"/>
+            <input type="reset" value="CANCEL" name="CANCEL"/>
+            <input type="submit" value="DONE" name="DONE"/>
+            
+            </div>
+
+        );
+    }
+}
+
+export default Settings;
