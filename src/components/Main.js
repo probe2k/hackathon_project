db95d980c9ddf669d746456a11fc9c43943e96e6
ha-wk <ayushkr0369@gmail.com>
2022-06-19 16:54:31 +0530

    route added for settings components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Login from './Login';
+import Settings from './settings'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Main = () => (
     <Router>
         <Routes>
             <Route exact path="/" element={<Login />} />
+            <Route exact path="/settings" element={<Settings />} />
         </Routes>
     </Router>
 );
